547a8cb9a05387023bb5c62134392988d815998b
Staffell <staffell123@gmail.com>
2025-01-22 20:09:24 -0600

    Change expression from Entity to Living Entity

--- src/main/java/com/staffell/SkFreaky/elements/conditions/CondIsFreaky.java
+++ src/main/java/com/staffell/SkFreaky/elements/conditions/CondIsFreaky.java
@@ -8,6 +8,7 @@ import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.util.Kleenean;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Animals;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.Event;
 import org.eclipse.jdt.annotation.Nullable;
 import org.jetbrains.annotations.NotNull;
@@ -25,13 +26,13 @@ public class CondIsFreaky extends Condition {
                 "%livingentity% is(n't| not) [currently] [getting] freaky");
     }
 
-    private Expression<Entity> entityExpr;
+    private Expression<LivingEntity> entityExpr;
     private boolean negate;
 
     @Override
     @SuppressWarnings("unchecked")
     public boolean init(Expression<?> @NotNull [] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
-        entityExpr = (Expression<Entity>) exprs[0];
+        entityExpr = (Expression<LivingEntity>) exprs[0];
         negate = matchedPattern == 1;
         return true;
     }
